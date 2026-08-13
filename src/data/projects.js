342d8aaa920955d7faa2a0acc7a4a0200bf73522
squilliam12 <wfan@wm.edu>
2026-08-13 12:25:00 -0500

    Add EM to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,7 +12,19 @@ export const projects = [
 
         link: "https://github.com/squilliam34/gravity/tree/main",
     },
+    {
+        title: "Explaining Markets",
+        description:
+            "An ongoing competition that tests how well AI agents can predict asset price movements from earnings calls in real-time.",
 
+        technologies: [
+            "Python",
+            "Agentic AI",
+            "Asset Pricing",
+        ],
+
+        link: "https://github.com/william-fan-1/explaining-markets",
+    },
     {
         title: "Volatility Surface",
         description:
